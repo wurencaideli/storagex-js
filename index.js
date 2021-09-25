7f4be3f5f9e25c0a55b58b755f1582ee44773765
吴哈哈 <62051421+wurencaideli@users.noreply.github.com>
2021-09-25 16:17:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-    storageX storage对象化储存
+    storagex-js storage对象化储存
     不支持function的储存
     不支持对象的循环迭代
     支持的储存模式 local(默认) session uni(uniapp) wx(微信小程序)
@@ -8,3 +8,6 @@
 /*jshint esversion: 9 */
 //可以根据自己的需求导出那些函数
 export * from "./src/StorageX";
+export {
+    deepRemoveProxy,
+} from "./src/common/Tools";
